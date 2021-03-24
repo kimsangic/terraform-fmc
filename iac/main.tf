c3dd2d9e215a5cda5ddf8d6cfe8fea10131bfcef
Everette McConnell <bmcconnell@Everettes-MacBook-Pro.local>
2021-03-24 15:38:35 -0400

    new pool

--- iac/main.tf
+++ iac/main.tf
@@ -12,7 +12,6 @@ locals {
   }
 }
 
-
 # Create a Virtual Network within the Resource Group
 resource "azurerm_virtual_network" "main" {
   name                = "${local.prefix}-vnet"
@@ -179,25 +178,6 @@ resource "azurerm_role_assignment" "blob_contributor" {
   principal_id          = azurerm_user_assigned_identity.managed_id.principal_id
 }
 
-output "private_key" {
-  value = tls_private_key.bootstrap_private_key.private_key_pem
-}
-
-output "vm_ip" {
-  value = data.azurerm_public_ip.pip.ip_address 
-}
-
-output "vm_rg" {
-  value = data.azurerm_resource_group.project-rg.name 
-}
-
-output "vm_name" {
-  value = azurerm_virtual_machine.vm.name
-}
-
-output "vm_datadisk" {
-  value = azurerm_virtual_machine.vm.storage_data_disk.0.name
-}
 
 # resource "azurerm_managed_disk" "data-disk" {
 #   name                  = "${local.prefix}-datadisk1"
